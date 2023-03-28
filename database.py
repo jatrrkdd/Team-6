baaf6628f45b60ee45fa0837726b69db6736b4e1
flapjakes <flapjakesxe@gmail.com>
2023-03-28 16:18:42 -0600

    Added search_employees add_employee and remove_employee Changed parts of employee

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 from threading import Thread
 
 import hashlib
+import re
 
 #example database employees with password "12345" and all fields
 #12-3456789,Bob,Tester,894 Glendale Plaza,Worcester,Massachusetts,1610,2,62862.15,46,16.85,123-456-7890,3/24/2023,123-45-6789,3/24/2023,30417353-K,465794-3611,0,assitant to the manager,stapler,False,4e4577f023d1c8ab0deb91ecc1cc4f51a0cd232e0b91e60c8811e6c82da60956,bob@bob.com
@@ -26,79 +27,185 @@ class Employee:
 		Attributes:
 		id, first_name, last_name, address, city, state, zip, classification,
 		salary, commissioned, hourly, office_phone, DOB, SSN, start_date,
-		bank_info, permission_level, title, dept,
+		route, account, permission_level, title, dept,
 		archived, password and email: all strings
 
 		Methods:
 	"""
 	#constructor
-	def __init__( self, database_line):
+	def __init__( self, id, first_name=None,last_name=None,address=None,city=None,state=None,zip=None,classification=None,
+	salary=None,commissioned=None,hourly=None,office_phone=None,DOB=None,SSN=None,
+	start_date=None,route=None,account=None,permission_level=None,title=None,dept=None,
+	archived=None,password=None,email=None):
+		"""all parameters should be strings"""
+		#set all fields to paramters
+		self.id = id
+		self.first_name = first_name
+		self.last_name = last_name
+		self.address = address
+		self.city = city
+		self.state = state
+		self.zip = zip
+		self.classification = classification
+		self.salary = salary
+		self.commissioned = commissioned
+		self.hourly = hourly
+		self.office_phone = office_phone
+		self.DOB = DOB
+		self.SSN = SSN
+		self.start_date = start_date
+		self.route = route
+		self.account = account
+		self.permission_level = permission_level
+		self.title = title
+		self.dept = dept
+		self.archived = archived
+		self.password = password #not hashed only hashed by edit employee
+		self.email = email
+	
+	def employee_from_line( database_line):
+		"""
+			set_fields_from_line(database_line)
+			takes in line from database and sets fields to
+			those in database_line
+		"""
 		#takes in line from employee.csv as argument
 		#split input line
 		fields = database_line.strip().split(",")
+
+		#if field has empty space set it to None. Empty fields are saved as " " in database
+		for i in range(len(fields)):
+			if fields[i] == " ":
+				fields[i] = None
+
 		#if split line doesn’t contain enough fields use old database format
 		if len(fields) == 11: #old database contains 11 fields
-			#set each attribute to corresponding index in split input line
-			self.id = fields[0]
-			self.first_name = fields[1]
-			self.last_name = fields[2]
-			self.address = fields[3]
-			self.city = fields[4]
-			self.state = fields[5]
-			self.zip = fields[6]
-			self.classification = fields[7]
-			self.salary = fields[8]
-			self.commissioned = fields[9]
-			self.hourly = fields[10]
-			self.office_phone = " "
-			self.DOB = " "
-			self.SSN = " "
-			self.start_date = " "
-			self.route = " "
-			self.account = " "
-			self.permission_level = 0
-			self.title = " "
-			self.dept = " "
-			self.archived = "False"
-			self.password = password_to_hash(self.id,"12345")
-			self.email = " "
+			#pass all fields to constructor, fields come in same order a parameters in constuctor
+			default_password = password_to_hash(fields[0],"12345")
+			return Employee(fields[0], fields[1],fields[2],
+			fields[3],fields[4],fields[5],
+			fields[6],fields[7],fields[8],
+			fields[9],fields[10],permission_level = 0,
+			archived = "False", password = default_password)
+
 		elif len(fields) == 23: #new database contains 22 fields
-			#set each attribute to corresponding index in split input line
-			self.id = fields[0]
-			self.first_name = fields[1]
-			self.last_name = fields[2]
-			self.address = fields[3]
-			self.city = fields[4]
-			self.state = fields[5]
-			self.zip = fields[6]
-			self.classification = fields[7]
-			self.salary = fields[8]
-			self.commissioned = fields[9]
-			self.hourly = fields[10]
-			self.office_phone = fields[11]
-			self.DOB = fields[12]
-			self.SSN = fields[13]
-			self.start_date = fields[14]
-			self.route = fields[15]
-			self.account = fields[16]
-			self.permission_level = fields[17]
-			self.title = fields[18]
-			self.dept = fields[19]
-			self.archived = fields[20]
-			self.password = fields[21]
-			self.email = fields[22]
+			#pass all fields to constructor, fields come in same order a parameters in constuctor
+			return Employee( fields[0],fields[1],fields[2],
+			 fields[3],fields[4],fields[5],
+			 fields[6],fields[7],fields[8],
+			 fields[9],fields[10],fields[11],
+			 fields[12],fields[13],fields[14],
+			 fields[15],fields[16],fields[17],
+			 fields[18],fields[19],fields[20],
+			 fields[21],fields[22])
 		else:
 			#unrecognized format
 			raise ValueError("Unrecognized database format")
+
 	def __str__(self):
 		#put all fields of employee into csv line
-		return (f"{self.id},{self.first_name},{self.last_name},"
-			    f"{self.address},{self.city},{self.state},{self.zip},{self.classification},"
-		        f"{self.salary},{self.commissioned},{self.hourly},{self.office_phone},{self.DOB},{self.SSN},"
-		        f"{self.start_date},{self.route},{self.account},{self.permission_level},{self.title},{self.dept},{self.archived},"
-		        f"{self.password},{self.email}")
+		return (f'{self.id           or " "},{self.first_name     or " "},{self.last_name        or " "},'
+			    f'{self.address      or " "},{self.city           or " "},{self.state            or " "},'
+				f'{self.zip          or " "},{self.classification or " "},{self.salary           or " "},'
+				f'{self.commissioned or " "},{self.hourly         or " "},{self.office_phone     or " "},'
+				f'{self.DOB          or " "},{self.SSN            or " "},{self.start_date       or " "},'
+				f'{self.route        or " "},{self.account        or " "},{self.permission_level or " "},'
+				f'{self.title        or " "},{self.dept           or " "},{self.archived         or " "},'
+		        f'{self.password     or " "},{self.email          or " "}')#the value or " " makes all None fields export as " "
+	
+	def update( self, updated_employee):
+		"""
+			sets self's fields to those in updated employee if not none
+			doesn't update id
+		"""
+		#check if given employee is valid
+		if not updated_employee.is_valid_employee():
+			return False #not valid do nothing
+
+		#update all fields if updated_employee does not have None for value
+		self.first_name = updated_employee.first_name or self.first_name
+		self.last_name = updated_employee.last_name or self.last_name
+		self.address = updated_employee.address or self.address
+		self.city = updated_employee.city or self.city
+		self.state = updated_employee.state or self.state
+		self.zip = updated_employee.zip or self.zip
+		self.classification = updated_employee.classification or self.classification
+		self.salary = updated_employee.salary or self.salary
+		self.commissioned = updated_employee.commissioned or self.commissioned
+		self.hourly = updated_employee.hourly or self.hourly
+		self.office_phone = updated_employee.office_phone or self.office_phone
+		self.DOB = updated_employee.DOB or self.DOB
+		self.SSN = updated_employee.SSN or self.SSN
+		self.start_date = updated_employee.start_date or self.start_date
+		self.route = updated_employee.route or self.route
+		self.account = updated_employee.account or self.account
+		self.permission_level = updated_employee.permission_level or self.permission_level
+		self.title = updated_employee.title or self.title
+		self.dept = updated_employee.dept or self.dept
+		self.archived = updated_employee.archived or self.archived
+		self.password = updated_employee.password or self.password
+		self.email = updated_employee.email or self.email
+
+		return True
+
+	def is_valid_employee(self):
+		"""is_valid_emloyee()
+		returns true if all fields are valid and false if not. None values are ignored"""
+		is_valid = True
+
+		return True #assumed valid for now valid tests are not done
+
+		if self.id and not re.search("",self.id):
+			return False #id is bad
+		if self.first_name and not isinstance(self.first_name,str):
+			return False #first_name is bad
+		if self.last_name and not isinstance(self.last_name,str):
+			return False #last_name is bad
+		if self.address and not isinstance(self.address,str):
+			return False #id is bad
+		if self.city and not isinstance(self.city,str):
+			return False #id is bad
+		if self.state and not isinstance(self.state,str):
+			return False #id is bad
+		if self.zip and not re.search("^\d{5}$",self.zip):
+			return False #zip is bad
+
+		if self.classification:
+			if not is_numeric(self.classification):
+				return False
+			if int(self.classification) > 2:
+				return False #class is bad
+		if not re.search("",self.salary):
+			return False #id is bad
+		if not re.search("",self.commissioned):
+			return False #id is bad
+		if not re.search("",self.hourly):
+			return False #id is bad
+		if not re.search("",self.office_phone):
+			return False #id is bad
+		if not re.search("",self.DOB):
+			return False #id is bad
+		if not re.search("",self.SSN):
+			return False #id is bad
+		if not re.search("",self.start_date):
+			return False #id is bad
+		if not re.search("",self.route):
+			return False #id is bad
+		if not re.search("",self.account):
+			return False #id is bad
+		if not re.search("",self.permission_level):
+			return False #id is bad
+		if not re.search("",self.title):
+			return False #id is bad
+		if not re.search("",self.dept):
+			return False #id is bad
+		if not re.search("",self.archived):
+			return False #id is bad
+		if not re.search("",self.email):
+			return False #id is bad
 
 
+		return is_valid
 
 
 #Database class
@@ -187,10 +294,12 @@ class Database():
 	returns employee object if login successful"""
 	def check_login( self, id, password):
 		#check if _load_employee_database thread is done
-		timeout = 5 #timeout 5 seconds database shouldn't take that long to load
+		self._load_employee_database.join(timeout=5)
+
 		if self._load_employee_database.is_alive():
-			#if loading isn't done wait
-			self._load_employee_database.join()
+			#if loading isn't done after timeout return None
+			return None
+
 		#loop through employees list
 		for employee in self.employees:
 			#if id matches employee
@@ -207,36 +316,98 @@ class Database():
 		#return None login does not match
 		return None
 
-	"""find_employees( name, id, title)
-	returns employee objects with matching name, id, and title. All are optional, just set unused to None."""
-	#create list of found employees
-	#loop through employees
-		#check if name matches or is substring
-			#if not continue
-		#check if id matches or is substring
-			#if not continue
-		#check if title matches or is substring
-			#if not continue
-		#after all checks employee matches filters add to list
-	#return found employees
+	def find_employees( self, name=None, id=None, title=None):
+		"""find_employees( name, id, title)
+		returns employee objects with matching name(first or last), id, and title. All are optional, just set unused to None.
+		Not case sensitive"""
+		#create list of found employees
+		found_employees = []
 
-	"""add_employee( employee)
-	adds new employee object to list"""
-	#loop through employees 
-		#check if employee id matches existing employee
-	#if does not match any existing add to employees list
+		#remove casing
+		if name:
+			name = name.lower()
+			
+		if title:
+			title = title.lower()
 
-	"""remove_employee( id):
-	removes employee with corresponding id"""
-	#loop through employees
-		#if id matches employee id then remove from employee list
-			#loop through time cards and receipts and remove any related records
+		#loop through employees
+		for employee in self.employees:
+			if name:
+				#check if name matches or is substring
+				in_first_name = name in employee.first_name.lower()
+				in_last_name = name in employee.last_name.lower()
+				if not (in_first_name or in_last_name):#if name isn't in first or last continue
+					#if not continue
+					continue
 
-	"""edit employee( employee):
-	updates the old employee data with data in employee"""
-	#loop through employees database
-		#if employee matches looped employee
-			#update attribute of looped employee to reflect changes
+			#check if id matches or is substring
+			if id:
+				if not id in employee.id:
+					#if not continue
+					continue
+
+			#check if title matches or is substring
+			if title:
+				#if employee title is none or title not in employee title
+				if not employee.title  or not title in employee.title.lower(): 
+					#if not continue
+					continue
+
+			#after all checks employee matches filters add to found list
+			found_employees.append(employee)
+
+		#return found employees
+		return found_employees
+
+	def add_employee( self, new_employee):
+		"""add_employee( employee)
+		adds new employee object to list does nothing if employee already exists(has same id)"""
+		#loop through employees
+		for employee in self.employees:
+			#check if employee id matches existing employee
+			if employee.id == new_employee.id:
+				return #two employees should not have the same id
+	
+		#if does not match any existing add to employees list
+		self.employees.append(new_employee)
+		
+		#save changes
+		self.save_database()
+
+	def remove_employee( self, id):
+		"""remove_employee( id):
+		removes employee with corresponding id"""
+
+		#loop through employees
+		for i in range(len(self.employees)):
+			#if id matches employee id then remove from employee list
+			if self.employees[i].id == id:
+				self.employees.pop(i)
+				#loop through time cards and receipts and remove any related records
+
+				self.save_database()#save changes
+
+				return #there should only be one employee matching id
+
+	def edit_employee( self, updated_employee):
+		"""edit employee( employee):
+		updates the old employee data with data in updated_employee
+		if field in updated_employee is None it is ignored. Use " " to clear field.
+		updated_employee must have id set to work
+		if password is given then password is hashed when set
+		returns True if all fields are valid False if one is invalid"""
+		#loop through employees database
+		for employee in self.employees:
+			#if employee matches looped employee
+			if employee.id == updated_employee.id:
+				#update attribute of looped employee to reflect changes
+			
+				if employee.update(updated_employee):#update returns false if updated_employee is invalid
+					#save changes
+					self.save_database()
+					return True
+				else:
+					return False
 
 
 	"""generate_pay_report( file_path, hourly, salaried, commissioned): creates pay report with data in database at file_path"""
@@ -292,7 +463,7 @@ class Database():
 			line = employee_file.readline()
 			if line != "":
 				#create new employee passing line to employee constructor
-				newEmployee = Employee(line)
+				newEmployee = Employee.employee_from_line(line)
 				#add employee to employees list
 				self.employees.append(newEmployee)
 		employee_file.close()
--- test_database.py
+++ test_database.py
@@ -1,5 +1,6 @@
 import database
 
+#pytest is used to run these
 
 def test_creation():
 	db = database.Database(None, "")
@@ -11,7 +12,28 @@ def test_login():
 	db = database.Database(None,"")
 	password_hash = database.password_to_hash("51-4678119","12345")
 	print(db.check_login("51-4678119","12345"))#assumes database has the id and password
-	
+
+def test_find():
+	db = database.Database(None,"")
+	db.check_login("12345","12345")#just calling this so the loader thread is done
+	found = db.find_employees("Bob")
+	assert(len(found) == 4) #assuming only 4 people have bob in name
+	found = db.find_employees(id="12-34")
+	assert(len(found) == 3) #assuming only 3 people have "12-34" in id
+	found = db.find_employees(title="software engineer")
+	assert(len(found)==1) #assuming only 1 person has that title
+
+def test_add_remove_():
+	db = database.Database(None,"")
+	db.check_login("1235","2345")#just calling this so the laoder thread is done
+	db.add_employee(database.Employee("12-3456792","Jimmy","Jimanthymee",title="The Jimster"))
+	db.add_employee(database.Employee("12-3456792","Jimmy","Jimanthymee",title="The Jimster"))#try adding two
+	found = db.find_employees(title="Jimster")
+	print(len(found))
+	assert(len(found)==1)
+	db.remove_employee("12-3456792")
+	found = db.find_employees(title="Jimster")
+	assert(len(found)==0)
 
 #example database employees with password "12345" everyone has password "12345"
 #12-3456789,Bob,Tester,894 Glendale Plaza,Worcester,Massachusetts,1610,2,62862.15,46,16.85,123-456-7890,3/24/2023,123-45-6789,3/24/2023,30417353-K,465794-3611,0,assitant to the manager,stapler,False,4e4577f023d1c8ab0deb91ecc1cc4f51a0cd232e0b91e60c8811e6c82da60956,bob@bob.com
